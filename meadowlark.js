8b6af0e02af533f1c68629e85d7fed86efdb092e
Joan Miralles Pàez <joanmirallespaez@MacBook-Pro-de-Joan.local>
2014-10-11 22:07:57 +0200

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,5 @@
+ var fortune = require('./lib/fortune.js');
+
  var express = require('express');
   var app = express();
  // set up handlebars view engine
@@ -17,9 +19,8 @@
 	 res.send('Meadowlark Travel');
  });
  app.get('/about', function(req, res){
-	 var randomFortune =
-		fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: randomFortune });
+ 	res.render('about', { fortune: fortune.getFortune() } );
+
  }); 
 
  
@@ -43,10 +44,3 @@
 
 
  
- var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
