d95bf556a86aee8eefe2075ae12e764675a8f3b1
Joan Miralles Pàez <joanmirallespaez@MacBook-Pro-de-Joan.local>
2014-10-12 17:58:00 +0200

    added QA environment

--- meadowlark.js
+++ meadowlark.js
@@ -1,46 +1,64 @@
  var fortune = require('./lib/fortune.js');
 
  var express = require('express');
-  var app = express();
+ var app = express();
  // set up handlebars view engine
  
  var handlebars = require('express3-handlebars')
-     .create({ defaultLayout:'main' });
-     app.engine('handlebars', handlebars.engine);
-     app.set('view engine', 'handlebars');
+ .create({ defaultLayout:'main' });
+ app.engine('handlebars', handlebars.engine);
+ app.set('view engine', 'handlebars');
  
  app.set('port', process.env.PORT || 3000);
 
  app.use(express.static(__dirname + '/public'));
 
 
- app.get('/', function(req, res){
-	 res.type('text/plain');
-	 res.send('Meadowlark Travel');
+ app.use(function(req, res, next){
+ 	res.locals.showTests = app.get('env') !== 'production' &&
+ 	req.query.test === '1';
+ 	next();
  });
- app.get('/about', function(req, res){
- 	res.render('about', { fortune: fortune.getFortune() } );
 
- }); 
+ app.get('/', function(req, res){
+ 	res.render('home');
+ });
+ app.get('/about', function(req, res) {
+ 	res.render('about', {
+ 		fortune: fortune.getFortune(),
+ 		pageTestScript: '/qa/tests-about.js'
+ 	} );
+ });
+
+ app.get('/tours/hood-river', function(req, res){
+ 	res.render('tours/hood-river');
+ });
+ app.get('/tours/oregon-coast', function(req, res){
+ 	res.render('tours/oregon-coast');
+ });
+ app.get('/tours/request-group-rate', function(req, res){
+ 	res.render('tours/request-group-rate');
+ });
 
  
  // custom 404 page
  app.use(function(req, res){
-	 res.type('text/plain');
-	 res.status(404);
-	 res.send('404 - Not Found');
+ 	res.type('text/plain');
+ 	res.status(404);
+ 	res.send('404 - Not Found');
  });
  // custom 500 page
  app.use(function(err, req, res, next){
-	 console.error(err.stack);
-	 res.type('text/plain');
-	 res.status(500);
-	 res.send('500 - Server Error');
+ 	console.error(err.stack);
+ 	res.type('text/plain');
+ 	res.status(500);
+ 	res.send('500 - Server Error');
  });
  app.listen(app.get('port'), function(){
-	 console.log( 'Express started on http://localhost:' +
-			 app.get('port') + '; press Ctrl-C to terminate.' );
+ 	console.log( 'Express started on http://localhost:' +
+ 		app.get('port') + '; press Ctrl-C to terminate.' );
  });
+
 
 
  
